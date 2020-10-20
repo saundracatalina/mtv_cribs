cfde51072033e4b980c49d5d77711b5fdefb4ef7
Saundra <saundracatalina@gmail.com>
2020-10-20 09:28:55 -0600

    Add passing assertion--renter defaults to nil

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -2,12 +2,16 @@ class Apartment
   attr_reader :number,
               :monthly_rent,
               :bathrooms,
-              :bedrooms
+              :bedrooms,
+              :renter
 
   def initialize(data)
     @number = data[:number]
     @monthly_rent = data[:monthly_rent]
     @bathrooms = data[:bathrooms]
     @bedrooms = data[:bedrooms]
+    @renter = nil
   end
+
+
 end
