d0c82c9e10d95d9edb8ef1b8697c73656b0562ee
Saundra <saundracatalina@gmail.com>
2020-10-20 09:26:42 -0600

    Add working test_can_add_renter_to_unit

--- test/apartment_test.rb
+++ test/apartment_test.rb
@@ -10,10 +10,21 @@ class ApartmentTest < Minitest::Test
       monthly_rent: 1200,
       bathrooms: 1,
       bedrooms: 1})
+
     assert_instance_of Apartment, unit1
     assert_equal "A1", unit1.number
     assert_equal 1200, unit1.monthly_rent
     assert_equal 1, unit1.bathrooms
     assert_equal 1, unit1.bedrooms
+  end
+
+  def test_can_add_renter_to_unit
+    unit1 = Apartment.new({
+      number: "A1",
+      monthly_rent: 1200,
+      bathrooms: 1,
+      bedrooms: 1})
+
+    assert_equal nil, unit1.renter 
   end
 end
