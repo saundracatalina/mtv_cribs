2a779b83d95feb84625da248383611887e67632c
Saundra <saundracatalina@gmail.com>
2020-10-20 09:40:45 -0600

    Add passing assert_instance_of test

---
+++ test/building_test.rb
@@ -0,0 +1,13 @@
+require "minitest/autorun"
+require "minitest/pride"
+require './lib/apartment'
+require './lib/renter'
+require './lib/building'
+
+class BuildingTest < Minitest::Test
+  def test_it_exists_and_attributes
+    building = Building.new
+
+    assert_instance_of Building, building
+  end
+end
